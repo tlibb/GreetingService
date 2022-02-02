2888fb3ac852c9a9213ed4d24ae658c4498d15ae
Libbrecht Tine (BT) <tine.libbrecht@hm.com>
2022-02-02 14:24:19 +0100

    added file

---
+++ GreetingService/GreetingService.Infrastructure/AppSettingsUserService.cs
@@ -0,0 +1,19 @@
+﻿using GreetingService.Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace GreetingService.Infrastructure
+{
+    public class AppSettingsUserService : IUserService
+    {
+        private IConfiguration _config;
+        public AppSettingsUserService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public bool IsValidUser(string username, string password)
+        {
+           return (_config["MyUserName"] == username && _config["MyPassWord"] == password);
+        }
+    }
+}
