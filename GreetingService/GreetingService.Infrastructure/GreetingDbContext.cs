122fd78f515183e0fecc1e87e67a13f766c9f300
Libbrecht Tine (BT) <tine.libbrecht@hm.com>
2022-02-24 16:12:10 +0100

    added a relation between users and greetings

--- GreetingService/GreetingService.Infrastructure/GreetingDbContext.cs
+++ GreetingService/GreetingService.Infrastructure/GreetingDbContext.cs
@@ -35,6 +35,23 @@ namespace GreetingService.Infrastructure
             //Tell EF Core that the primary key of User table is email
             modelBuilder.Entity<User>()
                 .HasKey(c => c.Email);
+
+            //Tell EF Core that Greeting.From is a foreign key for User table. 
+            //https://docs.microsoft.com/en-us/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-simple-key%2Csimple-key#without-navigation-property
+            modelBuilder.Entity<Greeting>()
+                .HasOne<User>()
+                .WithMany()
+                .HasForeignKey(x => x.From)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+            //Tell EF Core that Greeting.To is a foreign key for User table
+            modelBuilder.Entity<Greeting>()
+                .HasOne<User>()
+                .WithMany()
+                .HasForeignKey(x => x.To)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.ClientCascade);
         }
 
         public DbSet<Greeting> Greetings { get; set; }
